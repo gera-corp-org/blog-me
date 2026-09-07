989fc6732f468b9358a81fc193a087a4c9987463
gera-corp <exandore@gmail.com>
2026-09-07 17:49:42 +0200

    Fix: separate blocks with a space in makeExcerpt

--- src/domain/excerpt.js
+++ src/domain/excerpt.js
@@ -7,8 +7,15 @@ const ENTITIES = {
   '&nbsp;': ' ',
 };
 
+// Конец блока и перенос строки становятся пробелом, иначе соседние абзацы
+// склеятся в «Первый абзац.Второй абзац.». Остальные теги убираются
+// начисто, иначе перед точкой появится пробел.
+const BLOCK_BOUNDARY =
+  /<\/(p|div|h[1-6]|li|ul|ol|blockquote|pre|figure|figcaption|table|tr|td|th)>|<br\s*\/?>/gi;
+
 export function makeExcerpt(html, limit = 200) {
   const text = String(html ?? '')
+    .replace(BLOCK_BOUNDARY, ' ')
     .replace(/<[^>]*>/g, '')
     .replace(/&amp;|&lt;|&gt;|&quot;|&#39;|&nbsp;/g, (entity) => ENTITIES[entity])
     .replace(/\s+/g, ' ')
